40f36223affa324016e20419da25906084c7aa8e
Rofiqul-Islam <shourav95.ri@gmail.com>
2017-05-05 21:08:14 +0600

    login ok

--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 
 <?php
-
+session_start();
 $servername="localhost";
 $username = "virtualshelf";
 $password = "helloworld";
 $dbname = "virtualshelf";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$email = $_POST['username'];
-echo $email;
-$pass = $_POST['pass'];
-echo $pass;
+$email = $_POST['email'];
+
+//$pass = md5($_POST['pass']);
+$pass=$_POST['pass'];
+
 
 
 
@@ -21,7 +22,7 @@ if($conn->connect_error)
     
 }
     $sql ="SELECT count(*) FROM user WHERE(
-		        User_Name='" . $email . "' 
+		        User_Email='" . $email . "' 
 				AND 
 				Password='" . $pass . "');";
 
@@ -29,12 +30,25 @@ if($conn->connect_error)
     $result = $conn->query($sql);
     if($result->num_rows === 0)
     {
-        echo "0";
+        $_SESSION["flag"] = "0";
     }
     else
     {
-        echo "1";
+        $_SESSION["flag"] = "1";
     }
+    $sql1="SELECT User_ID from user WHERE(User_Email='" . $email . "' 
+				AND 
+				Password='" . $pass . "');";
+
+	$id=fetch_assoc($sql1);
+	echo $id;
+
+   if($_SESSION["flag"]=="0")
+   {
+
+   		echo "hello";
+
+   }
 
     $conn->close();
 
