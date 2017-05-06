e48e707a7d128015d4b56e72704664a3d71ea26b
rfnoman <rf.noman@yahoo.com>
2017-05-07 03:43:47 +0600

    like

--- shelve/data.php
+++ shelve/data.php
@@ -6,19 +6,20 @@ $books_name;
 $author_name;
 $var=$_GET['num'];
 $data="";
-$sql="select book_id, book_name, book_author, Image from book limit ".$var;
+$sql="select book_id, book_name, book_author, Image, book_like from book limit ".$var;
 $ch_image;
-
+$like;
 $result = mysqli_query($con,$sql);
+
 
 while($obj=mysqli_fetch_object($result)){
 $books_name = $obj->book_name;
 $author_name = $obj->book_author;
 $ch_image=$obj->Image;
 $book_id=$obj->book_id;
+$like=$obj->book_like;
 
-
-$data=$data."<li><a href=\"project-detail-page.php?data=".$book_id."\"><img src="."default.jpg"."><div class=\"overlay\"><summary><h2>".$books_name."</h2><h3>".$author_name."</h3></summary><><div class=\"loves\"><span>77</span></div></div></a></li>";
+$data=$data."<li><a href=\"project-detail-page.php?data=".$book_id."\"><img src="."default.jpg"."><div class=\"overlay\"><summary><h2>".$books_name."</h2><h3>".$author_name."</h3></summary><><div class=\"loves\"><span>".$like."</span></div></div></a></li>";
 
 
 if($var==0)break;
