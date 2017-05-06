601b590c0f40f12bf9b06d4cfecd6d112f3faf5e
rfnoman <rf.noman@yahoo.com>
2017-05-06 16:34:50 +0600

    msg

--- shelve/page.php
+++ shelve/page.php
@@ -3,7 +3,7 @@
 if(isset($_GET['id'])) {
 
     $book_id = $_GET['id'];
-    $con = mysqli_connect('localhost', 'root', '', 'webproject');
+    $con = mysqli_connect('localhost', 'virtualshelf', 'helloworld', 'virtualshelf');
     if ($con->connect_errno > 0) echo "not connected";
     $sql = "select book_name, book_author, edition, Image from book where book_id=" . $book_id;
     $result = mysqli_query($con, $sql);
