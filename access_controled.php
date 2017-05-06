6d1444aadd08aa3fc7748ed46d4794a7b6493330
Rofiqul-Islam <shourav95.ri@gmail.com>
2017-05-06 17:13:56 +0600

    session used

--- access_controled.php
+++ access_controled.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
-	if(isset($_SESSION['user_id']))
+	if(isset($_SESSION["user_id"]))
 	{
-		echo "Welcome ".$_SESSION['user_id'];
+		//echo "Welcome ".$_SESSION['user_id'];
 	}
 	else
 	{
