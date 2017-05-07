b9e3d512e79db7254ad03f5a48d54a973e1467a4
rfnoman <rf.noman@yahoo.com>
2017-05-07 19:25:03 +0600

    like

--- shelve/like.php
+++ shelve/like.php
@@ -4,7 +4,7 @@ $user_id=$_SESSION['user_id']
 $book_id=$_GET['id'];
 $con = mysqli_connect('localhost', 'virtualshelf', 'helloworld', 'virtualshelf');
     if ($con->connect_errno > 0) echo "not connected";
-    $sql = "update book set book_like =Book_like+1 where book_id=". $book_id;
+    $sql = "update book set book_like =book_like+1 where book_id=".$book_id;
 	$result = mysqli_query($con, $sql);
 	$sql = "select * from liked_books where User_ID=".$user_id."& book_id=".$book_id;
 	$result = mysqli_query($con, $sql);
