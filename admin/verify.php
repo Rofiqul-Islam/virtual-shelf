9901e46e8e26f9aba4fee78e96c4575b63ea4126
Rofiqul-Islam <shourav95.ri@gmail.com>
2017-05-08 03:44:21 +0600

    seminar

--- admin/verify.php
+++ admin/verify.php
@@ -7,8 +7,8 @@ echo $id;
 	  if($conn->connect_errno>0) echo "not connected";
 	  else echo "connected";
 
-	 $sql="UPDATE user
-	SET status = '1' WHERE User_Email='" . $email . "';";
+	 $sql="UPDATE admin
+	SET status = '1' WHERE email='" . $email . "';";
 	$result1 = $conn->query($sql);
 		$conn->close();
 
