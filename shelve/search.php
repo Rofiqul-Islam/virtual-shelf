047746202190cad5aa806cc34c1bcb6d90a7ffc5
rfnoman <rf.noman@yahoo.com>
2017-05-08 01:37:04 +0600

    like

--- shelve/search.php
+++ shelve/search.php
@@ -1,9 +1,8 @@
 <?php
-$item=$_GET['item'];
-echo $item;
-$con = mysqli_connect('localhost', 'root', '','webproject');
+
+$con = mysqli_connect('localhost', 'virtualshelf', 'helloworld','virtualshelf');
 if($con->connect_errno>0) echo "not connected";
-$sql="select book_name from book where book_name like '%".$item."%';";
+$sql="select book_id, book_name, book_author, Image, book_like from book where book_name like '".$item."%' limit 21";
 
 
 $data="";
@@ -11,7 +10,12 @@ $result = mysqli_query($con,$sql);
 
 while($obj=mysqli_fetch_object($result))
 {
-	$data=$data."<li><font size=\"3\">".$obj->book_name."</li>";
+	$books_name = $obj->book_name;
+	$author_name = $obj->book_author;
+	$ch_image=$obj->Image;
+	$book_id=$obj->book_id;
+	$like=$obj->book_like;
+	$data=$data."<li><a href=\"project-detail-page.php?data=".$book_id."\"><img src=".$ch_image."><h class =\"caption\">".$books_name."</h><div class=\"overlay\"><summary><h2>".$books_name."</h2><h3>".$author_name."</h3></summary><><div class=\"loves\"><span>".$like."</span></div></div></a></li>";
 	
 }	
 echo $data;
