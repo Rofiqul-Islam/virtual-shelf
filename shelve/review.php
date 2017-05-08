714f55a3656878ee5adbba2f79a224d0e0d0e305
rfnoman <rf.noman@yahoo.com>
2017-05-08 07:24:06 +0600

    msg

--- shelve/review.php
+++ shelve/review.php
@@ -7,15 +7,15 @@ if(isset($_GET['rev'])) {
     $rev= $_GET['rev'];
     $con = mysqli_connect('localhost', 'virtualshelf', 'helloworld', 'virtualshelf');
     if ($con->connect_errno > 0) echo "not connected";
-	$sql ="select User_Name from user where User_ID=32";
+	$sql ="select User_Name from user where User_ID=".$user_id;
 	$result = mysqli_query($con, $sql);
 	$obj=mysqli_fetch_object($result);
-	$user_name=obj->User_Name;
+	$user_name=$obj->User_Name;
 	
-    /*$sql ="INSERT INTO review(book_id, User_ID, Review,User_Name) VALUES (".$book_id.",".$user_id.",'".$rev."','".$user_name."')";
+    $sql ="INSERT INTO review(book_id, User_ID, Review,User_Name) VALUES (".$book_id.",".$user_id.",'".$rev."','".$user_name."')";
     $result = mysqli_query($con, $sql);
     
-*/
+
 	echo $rev." ".$book_id." ".$user_id." ".$user_name;
 }
 else echo "nothing";
