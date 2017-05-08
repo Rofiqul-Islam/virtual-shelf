91336725fa8d0b1b48698c29a0978a752820cc6d
rfnoman <rf.noman@yahoo.com>
2017-05-08 11:58:42 +0600

    msg

--- show_seminar/page.php
+++ show_seminar/page.php
@@ -5,7 +5,7 @@ if(isset($_GET['id'])) {
     $seminar_id = $_GET['id'];
     $con = mysqli_connect('localhost', 'virtualshelf', 'helloworld', 'virtualshelf');
     if ($con->connect_errno > 0) echo "not connected";
-    $sql = "select name, subject, speaker, place, date, listener from book where id=" .$seminar_id;
+    $sql = "select name, subject, speaker, place, date, listener from seminar where id=" .$seminar_id;
     $result = mysqli_query($con, $sql);
     $obj = mysqli_fetch_object($result);
     
